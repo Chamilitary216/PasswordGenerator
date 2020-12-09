9c01a0f3ba8d3d1fec7487f41fcd93dd398e02c4
Chris Curry <Chamilitary216@hotmail.com>
2020-12-09 00:02:16 -0500

    testing else if statement/alerts

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lowercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "0123456789"
+var sym = "!@#$%^&*_+"
+var numberCheck;
+var passwordLength;
+var uppercaseCheck;
+var specialCheck
 
-alert("Click Generate Password to start!");
 
-function prompt () {
-  var generateBtn = prompt("jhhjgkhgkhg");
-  alert (generateBtn);
+
+//Used to figure out length of password
+function determineLength() {
+  passwordLength = prompt("How many characters would you like your password to be? (Note: Must be between 8-128 characters)");
+
+  if (passwordLength< 8) {
+    alert("Password length must be a number between 8-128 characters");
+    determineLength();
+
+  }
+  
+  else if (passwordLength>128){
+  alert("Password length must be a number between 8-128 characters");
+  determineLength();
+}else if (isNaN(passwordLength)){
+  alert("Password length must be a number between 8-128 characters");
+  determineLength();
+return passwordLength;
 }
+
+
 
 // Write password to the #password input
 function writePassword() {
